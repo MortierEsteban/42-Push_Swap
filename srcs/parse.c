6b29d43dbfcab3571cdc1f818a3240c79f2ee5a2
Logan SIDAN <logan.sidan@icloud.com>
2022-01-10 17:23:28 +0100

    wow

--- srcs/parse.c
+++ srcs/parse.c
@@ -6,7 +6,7 @@
 /*   By: lsidan <lsidan@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/10 12:37:16 by lsidan            #+#    #+#             */
-/*   Updated: 2022/01/10 17:04:29 by lsidan           ###   ########.fr       */
+/*   Updated: 2022/01/10 17:20:26 by lsidan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,7 @@ int	ft_check(char **tmp)
 		j = i + 1;
 		while (tmp[j])
 		{
-			if (ft_strcmp(tmp[i], tmp[j]) == 0)
+			if (!ft_strcmp(tmp[i], tmp[j]))
 				ft_exit();
 			j++;
 		}
@@ -60,7 +60,7 @@ int	ft_isnan(char *s)
 	i = -1;
 	while (s[++i])
 	{
-		if (ft_isdigit(s[i]) == 0 && s[i] != ' ')
+		if (!ft_isdigit(s[i]) && s[i] != ' ')
 			return (0);
 	}
 	return (1);
@@ -79,9 +79,9 @@ t_stack	ft_char_to_int(int ac, char **av)
 	if (!s || ft_isnan(s) == 0)
 		ft_exit();
 	tmp = ft_split(s, ' ');
+	free(s);
 	if (ft_check(tmp))
 		ft_exit();
-	free(s);
 	while (tmp[i])
 		i++;
 	tab.size = i;
